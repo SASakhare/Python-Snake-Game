672458131d607c4b4f2bd9e313ac734818f500ba
sejal <sakharwsejal@gmail.com>
2026-03-28 19:30:22 +0530

    Moving block with timer

--- main.py
+++ main.py
@@ -2,46 +2,55 @@ import pygame
 import time
 from pygame.locals import *
 
-
+SIZE=40
 class Snake:
 
-    def __init__(self, game_screen, bg_color: tuple[int], Height: int, Weight: int):
+    def __init__(self, game_screen, bg_color: tuple[int], Height: int, Weight: int,length:int):
 
+        self.length=length
         self.game_screen = game_screen
         self.block = pygame.image.load("./resources/block.jpg").convert()
-        self.x, self.y = 100, 400
+        self.x, self.y =[SIZE]*self.length,[SIZE]*self.length
         self.bg_color = bg_color
         self.Height, self.Weight = Height, Weight
         self.dir = None
+        self.delta=40
 
     def draw(self):
 
         self.game_screen.fill(self.bg_color)
-        self.game_screen.blit(self.block, (self.x, self.y))
+        for x,y in zip(self.x,self.y):
+            self.game_screen.blit(self.block, (x, y))
         pygame.display.flip()
 
+
     def move_up(self):
-        self.y -= 5
-        self.y = max(self.y, 5)
+        self.y[0] -=self.delta
+        self.y[0] = max(self.y[0], 5)
         self.draw()
 
     def move_down(self):
-        self.y += 5
-        self.y = min(self.y, self.Height - 45)
+        self.y[0] += self.delta
+        self.y[0] = min(self.y[0], self.Height - 45)
         self.draw()
 
     def move_left(self):
-        self.x -= 5
-        self.x = max(self.x, 5)
+        self.x[0] -= self.delta
+        self.x[0] = max(self.x[0], 5)
         self.draw()
 
     def move_right(self):
-        self.x += 5
-        self.x = min(self.x, self.Weight - 45)
+        self.x[0] += self.delta
+        self.x[0] = min(self.x[0], self.Weight - 45)
         self.draw()
 
     def walk(self):
 
+        for i in range(self.length-1,0,-1):
+
+            self.x[i]=self.x[i-1]
+            self.y[i]=self.y[i-1]
+            
         if self.dir == "up":
             self.move_up()
         elif self.dir == "down":
@@ -63,7 +72,7 @@ class Game:
         self.surface = pygame.display.set_mode((self.Weight, self.Height))
         self.surface.fill(bg_color)
 
-        self.snake = Snake(self.surface, bg_color, Height, Weight)
+        self.snake = Snake(self.surface, bg_color, Height, Weight,10)
         self.snake.draw()
 
     def run(self):
@@ -94,7 +103,7 @@ class Game:
                     running = False
 
             self.snake.walk()
-            time.sleep(0.2)
+            time.sleep(0.5)
 
 
 # * game settings  :
