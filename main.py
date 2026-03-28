2d76d83a9198c2bcfada8d75e3664f2ae426305b
sejal <sakharwsejal@gmail.com>
2026-03-28 22:19:27 +0530

    Snake eats apple,find score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 from pygame.locals import *
+from random import randrange, choice
 
 SIZE = 40
 
@@ -19,6 +20,21 @@ class Apple:
         self.game_screen.blit(self.apple, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self, snake):
+
+        # Pick a random multiple of SIZE within [min, max]
+
+        while True:
+
+            x = choice(range(0, Weight - SIZE + 1, SIZE))
+            y = choice(range(0, Height - SIZE + 1, SIZE))
+
+            if (x, y) not in zip(snake.x, snake.y):
+                self.x, self.y = x, y
+                break
+
+        # print(f'x: {self.x} and y: {self.y}')
+
 
 class Snake:
 
@@ -34,6 +50,7 @@ class Snake:
         self.Height, self.Weight = Height, Weight
         self.dir = None
         self.delta = 40
+        self.last_x, self.last_y = self.x[-1], self.y[-1]
 
     def draw(self):
 
@@ -44,22 +61,22 @@ class Snake:
 
     def move_up(self):
         self.y[0] -= self.delta
-        self.y[0] = max(self.y[0], 5)
+        self.y[0] = max(self.y[0], 0)
         self.draw()
 
     def move_down(self):
         self.y[0] += self.delta
-        self.y[0] = min(self.y[0], self.Height - 45)
+        self.y[0] = min(self.y[0], self.Height - 40)
         self.draw()
 
     def move_left(self):
         self.x[0] -= self.delta
-        self.x[0] = max(self.x[0], 5)
+        self.x[0] = max(self.x[0], 0)
         self.draw()
 
     def move_right(self):
         self.x[0] += self.delta
-        self.x[0] = min(self.x[0], self.Weight - 45)
+        self.x[0] = min(self.x[0], self.Weight - 40)
         self.draw()
 
     def walk(self):
@@ -90,16 +107,56 @@ class Game:
         self.surface = pygame.display.set_mode((self.Weight, self.Height))
         self.surface.fill(bg_color)
 
-        self.snake = Snake(self.surface, bg_color, Height, Weight, 10)
+        self.snake = Snake(self.surface, bg_color, Height, Weight, 1)
         self.snake.draw()
 
         self.apple = Apple(self.surface)
         self.apple.draw()
+        self.score=0
 
     def play(self):
 
+        # self.display_score()
+        self.snake.last_x = self.snake.x[-1]
+        self.snake.last_y = self.snake.y[-1]
+
+        is_collide = self.is_collision(
+            self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y
+        )
+
+        if is_collide:
+            print(f"Collides")
+            self.snake.x.append(self.snake.last_x)
+            self.snake.y.append(self.snake.last_y)
+            self.snake.length += 1
+            self.score+=1
+            self.apple.move(self.snake) 
+
+        else:
+            print(f"Not Collides")
+
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+
+
+    def display_score(self):
+        
+        font=pygame.font.SysFont('arial',30)
+        score=font.render(f'Score :{self.score}',True,(255,0,0))
+        self.surface.blit(score,(Weight-120,0))
+        pygame.display.flip()
+
+
+    def is_collision(self, x1, y1, x2, y2):
+
+        # * (x1,y1) => snake
+        # * (x2,y2) => apple
+        if x1 == x2 and y1 == y2:
+
+            return True
+
+        return False
 
     def run(self):
 
@@ -129,12 +186,12 @@ class Game:
                     running = False
 
             self.play()
-            time.sleep(0.5)
+            time.sleep(0.2)
 
 
 # * game settings  :
 
-Weight, Height = 705, 505
+Weight, Height = 800, 600
 bg_color = (255, 255, 255)
 
 
